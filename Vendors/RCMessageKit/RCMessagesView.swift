5ed8f9427c0c664a36c6dc4ac5b0703c287a1b9c
Ankith Madadi <ankithm28@gmail.com>
2019-07-04 20:29:34 -0700

    Setting Up my repo

---
+++ Vendors/RCMessageKit/RCMessagesView.swift
@@ -0,0 +1,469 @@
+
+class RCMessagesView: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextViewDelegate {
+
+	@IBOutlet var viewTitle: UIView!
+	@IBOutlet var labelTitle1: UILabel!
+	@IBOutlet var labelTitle2: UILabel!
+	@IBOutlet var buttonTitle: UIButton!
+	@IBOutlet var tableView: UITableView!
+	@IBOutlet var viewLoadEarlier: UIView!
+	@IBOutlet var viewTypingIndicator: UIView!
+	@IBOutlet var viewInput: UIView!
+	@IBOutlet var buttonInputAttach: UIButton!
+	@IBOutlet var textInput: UITextView!
+	@IBOutlet var buttonInputSend: UIButton!
+
+	private var initialized = false
+	private var centerView = CGPoint.zero
+
+// Intializing RCMessagesView
+    
+	convenience init() {
+
+		self.init(nibName: "RCMessagesView", bundle: nil)
+	}
+
+//Functions
+    
+	override func viewDidLoad() {
+
+		super.viewDidLoad()
+
+		navigationItem.titleView = viewTitle
+		tableView.register(RCSectionHeaderCell.self, forCellReuseIdentifier: "RCSectionHeaderCell")
+		tableView.register(RCBubbleHeaderCell.self, forCellReuseIdentifier: "RCBubbleHeaderCell")
+		tableView.register(RCBubbleFooterCell.self, forCellReuseIdentifier: "RCBubbleFooterCell")
+		tableView.register(RCSectionFooterCell.self, forCellReuseIdentifier: "RCSectionFooterCell")
+		tableView.register(RCTextMessageCell.self, forCellReuseIdentifier: "RCTextMessageCell")
+		tableView.register(RCEmojiMessageCell.self, forCellReuseIdentifier: "RCEmojiMessageCell")
+		tableView.register(RCPictureMessageCell.self, forCellReuseIdentifier: "RCPictureMessageCell")
+		tableView.register(RCVideoMessageCell.self, forCellReuseIdentifier: "RCVideoMessageCell")
+		tableView.register(RCAudioMessageCell.self, forCellReuseIdentifier: "RCAudioMessageCell")
+		tableView.register(RCLocationMessageCell.self, forCellReuseIdentifier: "RCLocationMessageCell")
+		tableView.register(RCStatusCell.self, forCellReuseIdentifier: "RCStatusCell")
+		tableView.tableHeaderView = viewLoadEarlier
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+		inputPanelInit()
+	}
+	override func viewDidLayoutSubviews() {
+
+		super.viewDidLayoutSubviews()
+
+		centerView = view.center
+
+		inputPanelUpdate()
+	}
+	override func viewWillAppear(_ animated: Bool) {
+
+		super.viewWillAppear(animated)
+
+		dismissKeyboard()
+	}
+	override func viewDidAppear(_ animated: Bool) {
+
+		super.viewDidAppear(animated)
+
+		if (initialized == false) {
+			initialized = true
+			scroll(toBottom: true)
+		}
+
+		centerView = view.center
+	}
+	override func viewWillDisappear(_ animated: Bool) {
+
+		super.viewWillDisappear(animated)
+
+		dismissKeyboard()
+	}
+    
+// Loading the Previous Functions/Methods
+    
+	func loadEarlierShow(_ show: Bool) {
+
+		viewLoadEarlier.isHidden = !show
+		var frame: CGRect = viewLoadEarlier.frame
+		frame.size.height = show ? 50 : 0
+		viewLoadEarlier.frame = frame
+		tableView.reloadData()
+	}
+
+// Message Methods recalled
+    
+	func rcmessage(_ indexPath: IndexPath) -> RCMessage {
+
+		return RCMessage()
+	}
+
+// Avatar Methods recalled
+
+	func avatarInitials(_ indexPath: IndexPath) -> String {
+
+		return ""
+	}
+	func avatarImage(_ indexPath: IndexPath) -> UIImage? {
+
+		return nil
+	}
+
+// Header footer Methods recalled
+	
+	func textSectionHeader(_ indexPath: IndexPath) -> String? {
+
+		return nil
+	}
+	func textBubbleHeader(_ indexPath: IndexPath) -> String? {
+
+		return nil
+	}
+	func textBubbleFooter(_ indexPath: IndexPath) -> String? {
+
+		return nil
+	}
+	func textSectionFooter(_ indexPath: IndexPath) -> String? {
+
+		return nil
+	}
+    
+//Controller Methods(I think menu)
+    
+	func menuItems(_ indexPath: IndexPath) -> [Any]? {
+
+		return nil
+	}
+	override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+
+		return false
+	}
+	override var canBecomeFirstResponder: Bool {
+
+		return true
+	}
+
+// Indicator methods
+
+	func typingIndicatorShow(_ show: Bool, animated: Bool) {
+
+		if show {
+			tableView.tableFooterView = viewTypingIndicator
+			scroll(toBottom: animated)
+		} else {
+			UIView.animate(withDuration: animated ? 0.25 : 0, animations: {
+				self.tableView.tableFooterView = nil
+			})
+		}
+	}
+	func typingIndicatorUpdate() {
+
+	}
+    
+//Keyboard emojis
+    
+	@objc func keyboardShow(_ notification: Notification?) {
+
+		if let info = notification?.userInfo {
+			if let keyboard = info[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+				let duration = TimeInterval(info[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double)
+				UIView.animate(withDuration: duration, delay: 0, options: .allowUserInteraction, animations: {
+					self.view.center = CGPoint(x: self.centerView.x, y: self.centerView.y - keyboard.size.height + self.view.safeAreaInsets.bottom)
+				})
+			}
+		}
+		UIMenuController.shared.menuItems = nil
+	}
+    
+	@objc func keyboardHide(_ notification: Notification?) {
+
+		if let info = notification?.userInfo {
+			let duration = TimeInterval(info[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double)
+			UIView.animate(withDuration: duration, delay: 0, options: .allowUserInteraction, animations: {
+				self.view.center = self.centerView
+			})
+		}
+	}
+    
+	func dismissKeyboard() {
+
+		view.endEditing(true)
+	}
+
+//Input panel methods
+
+	func inputPanelInit() {
+
+		viewInput.backgroundColor = RCMessages().inputViewBackColor
+		textInput.backgroundColor = RCMessages().inputTextBackColor
+
+		textInput.font = RCMessages().inputFont
+		textInput.textColor = RCMessages().inputTextTextColor
+
+		textInput.textContainer.lineFragmentPadding = 0
+		textInput.textContainerInset = RCMessages().inputInset
+
+		textInput.layer.borderColor = RCMessages().inputBorderColor
+		textInput.layer.borderWidth = RCMessages().inputBorderWidth
+
+		textInput.layer.cornerRadius = RCMessages().inputRadius
+		textInput.clipsToBounds = true
+	}
+    
+	func inputPanelUpdate() {
+
+		let heightView: CGFloat = view.frame.size.height
+		let widthView: CGFloat = view.frame.size.width
+
+		let leftSafe: CGFloat = view.safeAreaInsets.left
+		let rightSafe: CGFloat = view.safeAreaInsets.right
+		let bottomSafe: CGFloat = view.safeAreaInsets.bottom
+
+		let widthText: CGFloat = textInput.frame.size.width
+		var heightText: CGFloat
+		let sizeText = textInput.sizeThatFits(CGSize(width: widthText, height: CGFloat.greatestFiniteMagnitude))
+
+		heightText = CGFloat.maximum(RCMessages().inputTextHeightMin, sizeText.height)
+		heightText = CGFloat.minimum(RCMessages().inputTextHeightMax, heightText)
+
+		let heightInput: CGFloat = heightText + (RCMessages().inputViewHeightMin - RCMessages().inputTextHeightMin)
+
+		tableView.frame = CGRect(x: leftSafe, y: 0, width: widthView - leftSafe - rightSafe, height: heightView - bottomSafe - heightInput)
+
+		var frameViewInput: CGRect = viewInput.frame
+		frameViewInput.origin.y = heightView - bottomSafe - heightInput
+		frameViewInput.size.height = heightInput
+		viewInput.frame = frameViewInput
+
+		viewInput.layoutIfNeeded()
+
+		var frameAttach: CGRect = buttonInputAttach.frame
+		frameAttach.origin.y = heightInput - frameAttach.size.height
+		buttonInputAttach.frame = frameAttach
+
+		var frameTextInput: CGRect = textInput.frame
+		frameTextInput.size.height = heightText
+		textInput.frame = frameTextInput
+
+		var frameSend: CGRect = buttonInputSend.frame
+		frameSend.origin.y = heightInput - frameSend.size.height
+		buttonInputSend.frame = frameSend
+
+		buttonInputSend.isEnabled = textInput.text.count != 0
+
+		let offset = CGPoint(x: 0, y: sizeText.height - heightText)
+		textInput.setContentOffset(offset, animated: false)
+
+		scroll(toBottom: false)
+	}
+
+//UI Things
+
+	@IBAction func actionTitle(_ sender: Any) {
+
+		actionTitle()
+	}
+	func actionTitle() {
+
+	}
+
+// UI Responses
+
+	@IBAction func actionLoadEarlier(_ sender: Any) {
+
+		actionLoadEarlier()
+	}
+
+	func actionLoadEarlier() {
+
+	}
+	func actionTapBubble(_ indexPath: IndexPath) {
+
+	}
+
+	func actionTapAvatar(_ indexPath: IndexPath) {
+
+	}
+
+	@IBAction func actionInputAttach(_ sender: Any) {
+
+		actionAttachMessage()
+	}
+
+	@IBAction func actionInputSend(_ sender: Any) {
+
+		if (textInput.text.count != 0) {
+			actionSendMessage(textInput.text)
+			dismissKeyboard()
+			textInput.text = nil
+			inputPanelUpdate()
+		}
+	}
+
+	func actionAttachMessage() {
+
+	}
+
+	func actionSendMessage(_ text: String) {
+
+	}
+
+	func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+
+		dismissKeyboard()
+	}
+
+// Data
+    
+	func numberOfSections(in tableView: UITableView) -> Int {
+
+		return 0
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+		return 5
+	}
+
+	func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+
+		return RCMessages().sectionHeaderMargin
+	}
+
+	func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+
+		return RCMessages().sectionFooterMargin
+	}
+
+	func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+
+		view.tintColor = UIColor.clear
+	}
+
+	func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+
+		view.tintColor = UIColor.clear
+	}
+
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+		if (indexPath.row == 0) {
+			return RCSectionHeaderCell.height(indexPath, messagesView: self)
+		}
+
+		if (indexPath.row == 1) {
+			return RCBubbleHeaderCell.height(indexPath, messagesView: self)
+		}
+
+		if (indexPath.row == 2) {
+
+			let rcmessage = self.rcmessage(indexPath)
+			if (rcmessage.type == RC_TYPE_STATUS)	{ return RCStatusCell.height(indexPath, messagesView: self)				}
+			if (rcmessage.type == RC_TYPE_TEXT)		{ return RCTextMessageCell.height(indexPath, messagesView: self)		}
+			if (rcmessage.type == RC_TYPE_EMOJI)	{ return RCEmojiMessageCell.height(indexPath, messagesView: self)		}
+			if (rcmessage.type == RC_TYPE_PICTURE)	{ return RCPictureMessageCell.height(indexPath, messagesView: self)		}
+			if (rcmessage.type == RC_TYPE_VIDEO)	{ return RCVideoMessageCell.height(indexPath, messagesView: self)		}
+			if (rcmessage.type == RC_TYPE_AUDIO)	{ return RCAudioMessageCell.height(indexPath, messagesView: self)		}
+			if (rcmessage.type == RC_TYPE_LOCATION)	{ return RCLocationMessageCell.height(indexPath, messagesView: self)	}
+		}
+
+		if (indexPath.row == 3) {
+			return RCBubbleFooterCell.height(indexPath, messagesView: self)
+		}
+
+		if (indexPath.row == 4) {
+			return RCSectionFooterCell.height(indexPath, messagesView: self)
+		}
+		return 0
+	}
+
+// Functions for the table
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+		if (indexPath.row == 0) {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "RCSectionHeaderCell", for: indexPath) as! RCSectionHeaderCell
+			cell.bindData(indexPath, messagesView: self)
+			return cell
+		}
+
+		if (indexPath.row == 1) {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "RCBubbleHeaderCell", for: indexPath) as! RCBubbleHeaderCell
+			cell.bindData(indexPath, messagesView: self)
+			return cell
+		}
+
+		if (indexPath.row == 2) {
+			let rcmessage = self.rcmessage(indexPath)
+			if (rcmessage.type == RC_TYPE_STATUS) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCStatusCell", for: indexPath) as! RCStatusCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_TEXT) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCTextMessageCell", for: indexPath) as! RCTextMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_EMOJI) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCEmojiMessageCell", for: indexPath) as! RCEmojiMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_PICTURE) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCPictureMessageCell", for: indexPath) as! RCPictureMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_VIDEO) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCVideoMessageCell", for: indexPath) as! RCVideoMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_AUDIO) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCAudioMessageCell", for: indexPath) as! RCAudioMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+			if (rcmessage.type == RC_TYPE_LOCATION) {
+				let cell = tableView.dequeueReusableCell(withIdentifier: "RCLocationMessageCell", for: indexPath) as! RCLocationMessageCell
+				cell.bindData(indexPath, messagesView: self)
+				return cell
+			}
+		}
+
+		if (indexPath.row == 3) {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "RCBubbleFooterCell", for: indexPath) as! RCBubbleFooterCell
+			cell.bindData(indexPath, messagesView: self)
+			return cell
+		}
+
+		if (indexPath.row == 4) {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "RCSectionFooterCell", for: indexPath) as! RCSectionFooterCell
+			cell.bindData(indexPath, messagesView: self)
+			return cell
+		}
+
+		return UITableViewCell()
+	}
+
+
+	func scroll(toBottom animated: Bool) {
+
+		if (tableView.numberOfSections > 0) {
+			let indexPath = IndexPath(row: 0, section: tableView.numberOfSections - 1)
+			tableView.scrollToRow(at: indexPath, at: .top, animated: animated)
+		}
+	}
+
+    
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+
+		return true
+	}
+
+	func textViewDidChange(_ textView: UITextView) {
+
+		inputPanelUpdate()
+		typingIndicatorUpdate()
+	}
+}
